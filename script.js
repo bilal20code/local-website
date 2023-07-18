6cbb7aec4e4341f0b2c48adbcb6f50440cab0ba3
Muhammad Bilal Ahmad <111138242+bilalattari407@users.noreply.github.com>
2023-07-18 17:26:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+
+// Example code for displaying a pop-up alert on button click
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked!');
+  });
+  
+  // Additional JavaScript functionality can be added as per your requirements
+  
